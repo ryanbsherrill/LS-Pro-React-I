3dfc405b4a8339856d672cd67b4a27680d4f25cc
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-06-30 06:02:10 -0400

    initial commit

---
+++ profile/src/Body.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return (
+      <p>
+        Hi. My name is Ryan and this is my first
+        React App. I think I'm getting the hang
+        of it already. It's nothing really, if
+        you've ever used NodeJS before, as it's
+        just a simplified, front-end version of
+        the same thing. 
+      </p>
+    );
+  }
+}
+
+export default Body;
---
+++ profile/src/NavBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <h1>Ryan's First React App</h1>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
